58448dc3b8808ae3116b82b78854d706a5020a84
Igor Guimarães <62394816+igorguimaraessilva@users.noreply.github.com>
2021-10-07 14:08:20 -0300

    Criação de documentos pdf

---
+++ pdf.py
@@ -0,0 +1,27 @@
+from reportlab.pdfgen import canvas
+
+def GeneratePDF(lista):
+    try :
+        
+        nome_pdf = input('Informe o nome do PDF: ')
+        pdf = canvas.Canvas('{}.pdf'.format(nome_pdf))
+        x = 720
+        
+        for nome,data in lista.items():
+            
+            x -= 20
+            pdf.drawString(247,x, '{} : {}'.format(nome,data))
+        
+        pdf.setTitle(nome_pdf)
+        pdf.setFont("Helvetica-Oblique", 14)    
+        #pdf.drawString(247,700, 'nome e data')
+        pdf.setFont("Helvetica-Bold", 12)
+        #pdf.drawString(245,724, 'Nome e data')
+        pdf.save()
+        print('{}.pdf criado com suceso!'.format(nome_pdf))
+    except :
+        print('Erro ao gerar {}.pdf'.format(nome_pdf))
+
+lista = {'Manual de instalação do UltraStudio': '04/10/2021'}
+
+GeneratePDF(lista)
